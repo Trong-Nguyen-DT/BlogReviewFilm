f3eb6673cd7f92ac4d01ded5c5df30a61f236984
Trong-Nguyen-DT <trongnguyen.2002.nguyenvan@gmail.com>
2024-03-14 09:47:33 +0700

    second commit

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import AdminDashboard from './components/adminpage/admindashboard/AdminDashBoard';
-import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import BlogDetail from './components/adminpage/blogdetail/BlogDetail';
-import CreateBlog from './components/adminpage/createblog/CreateBlog';
-import Login from './components/login/Login';
+import HomePage from './pages/Guest/HomePage';
 
 const App = () => {
   return (
     <>
-      <Login />
+      <HomePage />
     </>
   );
 }
---
+++ FrontEnd/frontend/src/configs/config.js
@@ -0,0 +1,4 @@
+// // config.js
+const appURL = 'http://34.202.165.249';
+
+export default appURL;
